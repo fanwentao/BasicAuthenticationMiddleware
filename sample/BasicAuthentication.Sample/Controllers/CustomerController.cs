edfe3241bb4ebe2548b052f890d5baaca8aa3c46
yuye <a421295298@hotmail.com>
2017-05-19 00:22:39 +0800

    Migration

---
+++ sample/BasicAuthentication.Sample/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using System.Web.Http;
+
+namespace BasicAuthentication.Sample.Controllers
+{
+    [Authorize]
+    public class CustomerController : ApiController
+    {
+        public string Get()
+        {
+            var claim = (User as ClaimsPrincipal).FindFirst(ClaimTypes.NameIdentifier);
+
+            return claim.Value;
+        }
+    }
+}
---
+++ sample/BasicAuthentication.Sample/OwinStartup.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Security.Claims;
+using BasicAuthenticationMiddleware;
+
+[assembly: OwinStartup(typeof(BasicAuthentication.Sample.OwinStartup))]
+
+namespace BasicAuthentication.Sample
+{
+    public class OwinStartup
+    {
+        public static void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("Default", "{controller}/{customerID}", new { controller = "values", customerID = RouteParameter.Optional });
+            config.MapHttpAttributeRoutes();
+
+            app.UseBasicAuthentication(new BasicAuthenticationOptions
+            {
+                AuthenticateBasicCredentials = AuthenticateBasicCredentials,
+                Realm = "foo"
+            });
+
+            app.UseWebApi(config);
+
+        }
+
+        private static Task<ClaimsIdentity> AuthenticateBasicCredentials(BasicAuthenticationContext basicAuthenticationContext)
+        {
+            return Task.FromResult(new ClaimsIdentity(new[] { new Claim(ClaimTypes.NameIdentifier, "foo"), }, "Basic"));
+        }
+    }
+}
